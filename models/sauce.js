aa5b433b8dc92fedf217fb37740fcb197725a07d
skuilman <96374197+skuilman@users.noreply.github.com>
2022-04-17 02:35:04 +0200

    Add files via upload

---
+++ models/sauce.js
@@ -0,0 +1,19 @@
+// Requires
+const mongoose = require('mongoose');
+
+const sauceSchema = mongoose.Schema({
+  
+  userId: { type: String, required: true },
+    name: { type: String, required: true },
+    manufacturer:{type: String, required: true },
+    description: { type: String, required: true },
+    mainPepper:{type: String, required: true },
+    imageUrl: { type: String, required: true },
+    heat: { type: Number, required: true },
+    likes: { type: Number, required: true },
+    dislikes: { type: Number, required: true },
+    usersLiked:{type:[String], required: true },
+    usersDisliked:{type:[String], requried: true},
+});
+
+module.exports = mongoose.model('Sauce', sauceSchema);
